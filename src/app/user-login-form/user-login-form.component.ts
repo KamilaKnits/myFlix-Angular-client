81fbe0b442b0787a8494c617c398dfd8106ead26
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-07-24 10:37:49 -0400

    created user-login-form-component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './user-login-form.component.html',
   styleUrl: './user-login-form.component.scss'
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '' };
 
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) {}
+
+    ngOnInit(): void {
+    }
+
+    loginUser(): void {
+      this.fetchApiData.userLogin(this.userData).subscribe(
+        (response) => {
+          localStorage.setItem("user", JSON.stringify(response.user));
+          localStorage.setItem("token", response.token)
+        this.dialogRef.close();
+        this.snackBar.open('Logged in successfully!', 'OK', {
+          duration: 2000
+        });
+      }, (error) => {
+        console.error(error);
+        this.snackBar.open(error.error?.message ||'Login in Failed.', 'OK', {
+          duration: 2000
+        });
+      });
+    }
 }
